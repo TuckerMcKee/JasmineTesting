c21dfb12abbe0d6ffe1c953973967c4a43bd226b
Tucker McKee <tuckermckee@Tuckers-MacBook-Pro.local>
2024-06-27 12:14:24 -0500

    adding comment

--- calculator-test.js
+++ calculator-test.js
@@ -2,6 +2,8 @@
 it('should calculate the monthly rate correctly', function () {
   expect(calculateMonthlyPayment(getCurrentUIValues())).toEqual("665.87");
 });
+//I'm getting the following error: 
+//TypeError: Cannot read properties of null (reading 'value')
 
 
 it("should return a result with 2 decimal places", function() {
