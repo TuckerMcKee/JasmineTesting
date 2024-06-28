c98e4cd66bfbc8ca6e20171cc44caab5eccdb907
Tucker McKee <tuckermckee@Tuckers-MacBook-Pro.local>
2024-06-27 22:04:33 -0500

    adding second test

--- calculator-test.js
+++ calculator-test.js
@@ -7,12 +7,20 @@ it('should calculate the monthly rate correctly', function () {
   }
   expect(calculateMonthlyPayment(values)).toEqual("665.87");
 });
-//I'm getting the following error: 
-//TypeError: Cannot read properties of null (reading 'value')
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const amount = Math.random() * 50000;
+  const years = Math.random() * 30;
+  const rate = Math.random();
+  const values = {
+    "amount" : amount,
+    "years" : years,
+    "rate" : rate
+  }
+  console.log(values);
+  const monthlyPayment = calculateMonthlyPayment(values); 
+  expect(monthlyPayment * 1000 % 10).toEqual(0);
 });
 
 /// etc
